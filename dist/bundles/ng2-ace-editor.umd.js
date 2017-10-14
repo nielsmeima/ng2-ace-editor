70e058fd85d8bbf9f3912a95a935f8c5113e364f
Niels Meima <nielsmeima1@gmail.com>
2017-10-14 22:10:10 +0200

    save

--- dist/bundles/ng2-ace-editor.umd.js
+++ dist/bundles/ng2-ace-editor.umd.js
@@ -62,6 +62,7 @@ var AceEditorDirective = /** @class */ (function () {
     AceEditorDirective.prototype.emitCaretLocation = function () {
         var caret = this.editor.selection.getCursor();
         var location = this.editor.session.doc.positionToIndex(caret);
+        console.log("SourceCode: " + location);
         this.caretChange.emit(location);
     };
     Object.defineProperty(AceEditorDirective.prototype, "options", {
